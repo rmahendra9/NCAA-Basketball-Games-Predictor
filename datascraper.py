834cb1587b4547d3e0cbdf33a03c23e60b103936
Ritvik Mahendra <78041759+rmahendra9@users.noreply.github.com>
2022-10-17 02:17:05 -0500

    Edited workflow

--- datascraper.py
+++
@@ -1,5 +0,0 @@
-#
-# https://www.thepythoncode.com/article/convert-html-tables-into-csv-files-in-python
-#
-# Modified to extract NCAA College Basket Ball data from  https://www.sports-reference.com/cbb/
-#
