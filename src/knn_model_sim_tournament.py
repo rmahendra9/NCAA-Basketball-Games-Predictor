d54aaf767b5f30e8e0a8cc228b46cceefd3e773e
Ritvik Mahendra <78041759+rmahendra9@users.noreply.github.com>
2023-03-17 01:31:43 -0500

    Minor edits

--- src/knn_model_sim_tournament.py
+++ src/knn_model_sim_tournament.py
@@ -1,17 +1,6 @@
 import pandas as pd
 import math
-"""
-Sim score between games: A/B and C/D
-Take Sim(A,C), Sim(A,D), Sim(B,C), Sim(B,D)
-Weight(A/C) = Similarity Score(A,C) / (Similarity Score(A,C) + Similarity Score(A,D))
-Weight(A/D) = Similarity Score(A,D) / (Similarity Score(A,C) + Similarity Score(A,D)) 
-Weight(B/C) = Similarity Score(B,C) / (Similarity Score(B,C) + Similarity Score(B,D))
-Weight(B/D) = Similarity Score(B,D) / (Similarity Score(B,C) + Similarity Score(B,D))
-Unified Similarity Score = (Weight(A/C) * Similarity Score(A,C)) + (Weight(A/D) * Similarity Score(A,D)) + (Weight(B/C) * Similarity Score(B,C)) + (Weight(B/D) * Similarity Score(B,D))
 
-Option 2: (A,B) in a vector, just find distance to (C,D) (higher seed first) and do distance on pairs
-
-"""
 def add_data(df):
     df_2021 = pd.read_csv("NCAA_School_Stats_Tempo_Free_2021.csv")
     df_2019 = pd.read_csv("NCAA_School_Stats_Tempo_Free_2019.csv")
